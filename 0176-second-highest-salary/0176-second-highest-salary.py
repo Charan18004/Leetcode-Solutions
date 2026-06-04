99894be49a8385bd02f3febe993f07259797cb6f
Charan Kumar Reddy <charankumarreddyr085@gmail.com>
2026-06-04 19:24:58 +0530

    Time: 271 ms (33.05%), Space: 67 MB (84.86%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    salaries = employee["salary"].drop_duplicates().sort_values(ascending = False)
+    second_highest = salaries.iloc[1] if len(salaries) > 1 else None
+    return pd.DataFrame({"SecondHighestSalary": [second_highest]})
+    
